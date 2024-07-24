54f448496eb125b1b273a8b6c596ca37239c25bc
Nadir Hossain <nadirhossain336@gmail.com>
2024-07-24 17:57:39 +0600

    :art: news page

---
+++ src/app/(website)/news/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewsClient from './NewsClient'
+
+const page = () => {
+    return (
+        <React.Fragment>
+            <NewsClient/>
+        </React.Fragment>
+    )
+}
+
+export default page
