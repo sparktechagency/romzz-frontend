9520dd3b7148965f5358f69eb02287f972769495
Nadir Hossain <nadirhossain336@gmail.com>
2024-07-24 15:51:36 +0600

    :art: notification  page design

---
+++ src/app/(website)/notification/NotificationClient.tsx
@@ -0,0 +1,30 @@
+import Heading from '@/components/shared/Heading'
+import React from 'react'
+
+const NotificationClient = () => {
+    return (
+        <div className='container pt-6'>
+            <div className='flex items-center justify-between mb-6'>
+                <Heading name="Notifications" style="font-normal text-[32px] leading-[48px] text-primary"/>
+                <p className='underline text-secondary cursor-pointer'>Read All</p>
+            </div>
+            <div className='grid grid-cols-1 gap-4'>
+                {
+                    [...Array(6)].map((notification, index)=>{
+                        return(
+                            <div 
+                                className={`flex items-center justify-between rounded-lg p-6  ${index % 2 === 0 ? "bg-[#E6F2F5]" : "bg-[#F3F3F3]"}`} 
+                                key={index}
+                            >
+                                <p className='text-[#A1A1A1] text-[14px] leading-[16px] font-normal'>Yor post accepted for verification</p>
+                                <p className='text-[#A1A1A1] text-[14px] leading-[16px] font-normal'>03:00 PM, 20/10/2024</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default NotificationClient
---
+++ src/app/(website)/notification/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotificationClient from './NotificationClient'
+
+const page = () => {
+    return (
+        <React.Fragment>
+            <NotificationClient/>
+        </React.Fragment>
+    )
+}
+
+export default page
