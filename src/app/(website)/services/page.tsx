77de9cf6962f8e2407b98fb1b992f9a78efef749
Nadir Hossain <nadirhossain336@gmail.com>
2024-07-25 10:21:46 +0600

    :art: service page design complete

---
+++ src/app/(website)/services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServiceClient from './ServiceClient'
+
+const page = () => {
+    return (
+        <React.Fragment>
+            <ServiceClient/>
+        </React.Fragment>
+    )
+}
+
+export default page
